6155bf042d7e04938cbc24206edd720f6aa11bf2
Enrique Ortiz <enrique@carupi.com>
2019-06-19 18:07:42 -0400

    Pack everything in one folder

---
+++ ConsoleArgsParser/NTest/Program.cs
@@ -0,0 +1,138 @@
+﻿using CodeKatas;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace NTest
+{
+    //Group Only Bools
+    [TestFixture]
+    class BoolsTest
+    {
+        [Test]
+        public void EmptyScheme()
+        {
+            var scheme = new string[0];
+            var control = new ArgsClass<bool>();
+            Assert.Throws<NoSchemeException>(() => control.BoolArgs(scheme, null));
+        }
+        [Test]
+        public void OneScheme()
+        {
+            var scheme = new string[1];
+            scheme[0] = "-p";
+            var control = new ArgsClass<bool>();
+            var expected = new Dictionary<string, bool>();
+            expected.Add(scheme[0], false);
+
+            var actual = control.BoolArgs(scheme, null);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TwoScheme()
+        {
+            var scheme = new string[2];
+            scheme[0] = "-p";
+            scheme[1] = "-x";
+            var control = new ArgsClass<bool>();
+            var expected = new Dictionary<string, bool>();
+            expected.Add(scheme[0], false);
+            expected.Add(scheme[1], false);
+
+            var actual = control.BoolArgs(scheme, null);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void InvalidScheme()
+        {
+            var scheme = new string[2];
+            scheme[0] = "-px";
+            scheme[1] = "-x";
+            var control = new ArgsClass<bool>();
+            var expected = new Dictionary<string, bool>();
+            expected.Add(scheme[0], false);
+            expected.Add(scheme[1], false);
+
+            Assert.Throws<InvalidSchemeException>(() => control.BoolArgs(scheme, null));
+        }
+        [Test]
+        public void NoArgs()
+        {
+            var scheme = new string[2];
+            scheme[0] = "-p";
+            scheme[1] = "-x";
+            string arg = "-p";
+            var control = new ArgsClass<bool>();
+            var expected = new Dictionary<string, bool>();
+            expected.Add(scheme[0], true);
+            expected.Add(scheme[1], false);
+
+            Assert.AreEqual(expected, control.BoolArgs(scheme, arg));
+        }
+        [Test]
+        public void SomeArgs()
+        {
+            var scheme = new string[4];
+            scheme[0] = "-p";
+            scheme[1] = "-x";
+            scheme[2] = "-y";
+            scheme[3] = "-z";
+            string arg = "-p -y";
+            var control = new ArgsClass<bool>();
+            var expected = new Dictionary<string, bool>();
+            expected.Add(scheme[0], true);
+            expected.Add(scheme[1], false);
+            expected.Add(scheme[2], true);
+            expected.Add(scheme[3], false);
+            Assert.AreEqual(expected, control.BoolArgs(scheme, arg));
+        }
+        [Test]
+        public void InvalidArgs()
+        {
+            var scheme = new string[4];
+            scheme[0] = "-p";
+            scheme[1] = "-x";
+            scheme[2] = "-y";
+            scheme[3] = "-z";
+            string arg = "-p x";
+            var control = new ArgsClass<bool>();
+            var expected = new Dictionary<string, bool>();
+            expected.Add(scheme[0], true);
+            expected.Add(scheme[1], false);
+            expected.Add(scheme[2], true);
+            expected.Add(scheme[3], false);
+
+            Assert.Throws<InvalidArgException>(() => control.BoolArgs(scheme, arg));
+        }
+        [Test]
+        public void InvalidArgsNotDeclared()
+        {
+            var scheme = new string[4];
+            scheme[0] = "-p";
+            scheme[1] = "-x";
+            scheme[2] = "-y";
+            scheme[3] = "-z";
+            string arg = "-p -x -a";
+            var control = new ArgsClass<bool>();
+            var expected = new Dictionary<string, bool>();
+            expected.Add(scheme[0], true);
+            expected.Add(scheme[1], false);
+            expected.Add(scheme[2], true);
+            expected.Add(scheme[3], false);
+
+            Assert.Throws<InvalidArgException>(() => control.BoolArgs(scheme, arg));
+        }
+    }
+    [TestFixture]
+    class IntTests{
+        [Test]
+        public void SomeScheme()
+        {
+            var scheme = new string[0];
+            var control = new ArgsClass<bool>();
+            Assert.Throws<NoSchemeException>(() => control.BoolArgs(scheme, null));
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,66 @@
+# ConsoleArgsParser
+## CodeKatas
+Development tendency of apps course group project - CodeKatas, a library that helps you parse boolean flags from command line.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+Visual Studio 2017 or newer
+
+Nuget Package manager 
+
+NetCore extension installed
+
+### Installing
+
+Get Visual Studio IDE at https://visualstudio.microsoft.com/downloads/
+
+Start installer and leave everything as default (It will automatically install all the requierements)
+
+Clone this project on your local machine
+
+Start coding!!!!!!
+
+
+## Running the tests
+
+NUnit 
+NUnit 3 Test Adapter (Required for running unit tests in .NetCore)
+Microsoft.NET.Test.sdk (Development tools for running unit tests in .NetCore)
+
+### Break down into end to end tests
+
+No tests ready yet 
+
+### And coding style tests
+
+No explanation avaliable 
+
+## Built With
+
+* Powered by C# .NetCore 2.1
+
+## Contributing
+
+Please read [CONTRIBUTING.md]
+
+## Authors
+
+* **Development tendency of apps course students 
+
+See also the list of [contributors](https://github.com/Evalir/ConsoleArgsParser/graphs/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* This project was inspired by the Args Kata. You can find more information at http://codingdojo.org/kata/Args/
+
+## Blog Post
+
+[We wrote this blog post](https://medium.com/@ergerica76/intro-to-args-635d8a5f9ade?postPublishedType=initial) to introduce the tool. Check it out!
