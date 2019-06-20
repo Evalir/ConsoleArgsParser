2b5931d8677978c995cc5e49694aef5c4a181ecf
dreinix <ricardo.aar@outlook.com>
2019-06-19 21:19:00 -0400

    FinishedGenericParse
    -Se terminó la funcionalidad para adoptar los tipos de datos primarios.

--- ConsoleArgsParser/NTest/Program.cs
+++ ConsoleArgsParser/NTest/Program.cs
@@ -13,19 +13,19 @@ namespace NTest
         public void EmptyScheme()
         {
             var scheme = new string[0];
-            var control = new ArgsClass<bool>();
-            Assert.Throws<NoSchemeException>(() => control.ParseArgumentsWithSchema(scheme, null));
+            var control = new ArgsClass();
+            Assert.Throws<NoSchemeException>(() => control.BoolArgs(scheme, null));
         }
         [Test]
         public void OneScheme()
         {
             var scheme = new string[1];
             scheme[0] = "-p";
-            var control = new ArgsClass<bool>();
+            var control = new ArgsClass();
             var expected = new Dictionary<string, bool>();
             expected.Add(scheme[0], false);
 
-            var actual = control.ParseArgumentsWithSchema(scheme, null);
+            var actual = control.BoolArgs(scheme, null);
             Assert.AreEqual(expected, actual);
         }
 
@@ -35,12 +35,12 @@ namespace NTest
             var scheme = new string[2];
             scheme[0] = "-p";
             scheme[1] = "-x";
-            var control = new ArgsClass<bool>();
+            var control = new ArgsClass();
             var expected = new Dictionary<string, bool>();
             expected.Add(scheme[0], false);
             expected.Add(scheme[1], false);
 
-            var actual = control.ParseArgumentsWithSchema(scheme, null);
+            var actual = control.BoolArgs(scheme, null);
             Assert.AreEqual(expected, actual);
         }
 
@@ -50,12 +50,12 @@ namespace NTest
             var scheme = new string[2];
             scheme[0] = "-px";
             scheme[1] = "-x";
-            var control = new ArgsClass<bool>();
+            var control = new ArgsClass();
             var expected = new Dictionary<string, bool>();
             expected.Add(scheme[0], false);
             expected.Add(scheme[1], false);
 
-            Assert.Throws<InvalidSchemeException>(() => control.ParseArgumentsWithSchema(scheme, null));
+            Assert.Throws<InvalidSchemeException>(() => control.BoolArgs(scheme, null));
         }
         [Test]
         public void NoArgs()
@@ -64,12 +64,12 @@ namespace NTest
             scheme[0] = "-p";
             scheme[1] = "-x";
             string arg = "-p";
-            var control = new ArgsClass<bool>();
+            var control = new ArgsClass();
             var expected = new Dictionary<string, bool>();
             expected.Add(scheme[0], true);
             expected.Add(scheme[1], false);
 
-            Assert.AreEqual(expected, control.ParseArgumentsWithSchema(scheme, arg));
+            Assert.AreEqual(expected, control.BoolArgs(scheme, arg));
         }
         [Test]
         public void SomeArgs()
@@ -80,13 +80,13 @@ namespace NTest
             scheme[2] = "-y";
             scheme[3] = "-z";
             string arg = "-p -y";
-            var control = new ArgsClass<bool>();
+            var control = new ArgsClass();
             var expected = new Dictionary<string, bool>();
             expected.Add(scheme[0], true);
             expected.Add(scheme[1], false);
             expected.Add(scheme[2], true);
             expected.Add(scheme[3], false);
-            Assert.AreEqual(expected, control.ParseArgumentsWithSchema(scheme, arg));
+            Assert.AreEqual(expected, control.BoolArgs(scheme, arg));
         }
         [Test]
         public void InvalidArgs()
@@ -97,14 +97,14 @@ namespace NTest
             scheme[2] = "-y";
             scheme[3] = "-z";
             string arg = "-p x";
-            var control = new ArgsClass<bool>();
+            var control = new ArgsClass();
             var expected = new Dictionary<string, bool>();
             expected.Add(scheme[0], true);
             expected.Add(scheme[1], false);
             expected.Add(scheme[2], true);
             expected.Add(scheme[3], false);
 
-            Assert.Throws<InvalidArgException>(() => control.ParseArgumentsWithSchema(scheme, arg));
+            Assert.Throws<InvalidArgException>(() => control.BoolArgs(scheme, arg));
         }
         [Test]
         public void InvalidArgsNotDeclared()
@@ -115,25 +115,111 @@ namespace NTest
             scheme[2] = "-y";
             scheme[3] = "-z";
             string arg = "-p -x -a";
-            var control = new ArgsClass<bool>();
+            var control = new ArgsClass();
             var expected = new Dictionary<string, bool>();
             expected.Add(scheme[0], true);
             expected.Add(scheme[1], false);
             expected.Add(scheme[2], true);
             expected.Add(scheme[3], false);
 
-            Assert.Throws<InvalidArgException>(() => control.ParseArgumentsWithSchema(scheme, arg));
+            Assert.Throws<InvalidArgException>(() => control.BoolArgs(scheme, arg));
         }
     }
-
     [TestFixture]
-    class IntTests{
+    class GeneralTests{
         [Test]
-        public void SomeScheme()
+        public void boolScheme()
         {
-            var scheme = new string[0];
-            var control = new ArgsClass<bool>();
-            Assert.Throws<NoSchemeException>(() => control.ParseArgumentsWithSchema(scheme, null));
+            var scheme = new Dictionary<string, bool>();
+            scheme.Add("-p", new bool());
+            var control = new ArgsClass();
+            control.AddScheme(scheme);
+            var Expected = new Dictionary<string, string>();
+            Expected.Add("-p", "false");
+            Assert.AreEqual(Expected, control.GetSchemes());
+        }
+        [Test]
+        public void intScheme()
+        {
+            var scheme = new Dictionary<string, int>();
+            scheme.Add("-e", new int());
+            var control = new ArgsClass();
+            control.AddScheme<int>(scheme);
+            var Expected = new Dictionary<string, string>();
+            Expected.Add("-e", "");
+            Assert.AreEqual(Expected, control.GetSchemes());
+        }
+        [Test]
+        public void SomeTypeScheme()
+        {
+            var scheme = new Dictionary<string, int>();
+            scheme.Add("-e", new int());
+            var control = new ArgsClass();
+            control.AddScheme<int>(scheme);
+            
+            var scheme2 = new Dictionary<string, bool>();
+            scheme2.Add("-b", new bool());
+            control.AddScheme<bool>(scheme2);
+            var Expected = new Dictionary<string, string>();
+
+            Expected.Add("-e", "");
+            Expected.Add("-b", "false");
+            Assert.AreEqual(Expected, control.GetSchemes());
+        }
+        [Test]
+        public void ArgWithMoreThanOneType()
+        {
+            var Expected = new Dictionary<string, string>();
+
+            var control = new ArgsClass();
+            control.AddScheme<int>(new Dictionary<string, int>() { { "-e", new int() } });
+            control.AddScheme<bool>(new Dictionary<string, bool>() { { "-b", new bool() } });
+            string Arg = "-e 8080 -b";
+            Expected.Add("-e", "8080");
+            Expected.Add("-b", "true");
+            Assert.AreEqual(Expected, control.GeneralSParse(Arg));
+        }
+        [Test]
+        public void ArgWithMoreThanOneTypeII()
+        {
+            var Expected = new Dictionary<string, string>();
+
+            var control = new ArgsClass();
+            control.AddScheme<int>(new Dictionary<string, int>() { { "-e", new int() }, { "-d", new int() } });
+            control.AddScheme<bool>(new Dictionary<string, bool>() { { "-b", new bool() } });
+
+
+            string Arg = "-e 8080 -b -d 1250";
+            Expected.Add("-e", "8080");
+            Expected.Add("-d", "1250");
+            Expected.Add("-b", "true");
+            Assert.AreEqual(Expected, control.GeneralSParse(Arg));
+        }
+        [Test]
+        public void ArgWithMoreThanOneTypeAndTwoLessSign()
+        {
+            var Expected = new Dictionary<string, string>();
+
+            var control = new ArgsClass();
+            control.AddScheme<int>(new Dictionary<string, int>() { { "--e", new int() }, { "--d", new int() } });
+            control.AddScheme<bool>(new Dictionary<string, bool>() { { "--b", new bool() } });
+
+
+            string Arg = "--e 8080 --b --d 1250";
+            Expected.Add("--e", "8080");
+            Expected.Add("--d", "1250");
+            Expected.Add("--b", "true");
+            Assert.AreEqual(Expected, control.GeneralSParse(Arg));
+        }
+
+        [Test]
+        public void InvalidArgWithSomeSchemes()
+        {
+            var control = new ArgsClass();
+            control.AddScheme<int>(new Dictionary<string, int>() { { "-e", new int() }, { "-d", new int() } });
+            control.AddScheme<bool>(new Dictionary<string, bool>() { { "-b", new bool() } });
+            string Arg = "-e 8080 -b -d";
+            Assert.Throws<InvalidArgException>(() => control.GeneralSParse(Arg));
         }
     }
 }
